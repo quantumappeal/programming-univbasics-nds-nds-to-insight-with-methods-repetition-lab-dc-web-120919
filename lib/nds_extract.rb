350c5e3363dc97013b01b85f47e935b6969bd88b
Sean Beach <sean.beach1@yahoo.com>
2019-12-09 00:56:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,16 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+    source.map{ |d| d[:name] }
+  names = []
+  i = 0
+
+  while i < source.length do
+    names << source[i][:name]
+    i += 1
+  end
+
+  names
 end
 
 def total_gross(source)
